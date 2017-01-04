b25874216625126dfba98e34c270a8751e107246
jcraig4803 <jcraig4803@my.sctd.edu>
2017-01-03 21:47:13 -0500

    User Pick Number Win/Lose

--- CrashingCourse/Program.cs
+++ CrashingCourse/Program.cs
@@ -17,28 +17,44 @@ namespace CrashingCourse
             string user_name = "";
             string user_age = "";
             string user_phonenumber = "";
-            Console.WriteLine("Input Admin username");
-            user_input = Console.ReadLine();
-            Console.WriteLine("Input Admin password");
-            admin_log = Console.ReadLine();
-            if (user_input == admin_username && admin_log == admin_password)
+
+            string pick_number = "";
+            int winning_num = 2;
+            int input_num;
+
+            Console.WriteLine("Pick a Number 1 Throught 3");
+            pick_number = Console.ReadLine();
+            int.TryParse(pick_number, out input_num);
+            if (input_num == winning_num)
             {
-                Console.WriteLine("Tell me your name.");
-                user_name = Console.ReadLine();
 
-                Console.WriteLine("Hello, " + user_name + " Tell me your age, ");
-                user_age = Console.ReadLine();
+                Console.WriteLine("Input Admin username");
+                user_input = Console.ReadLine();
+                Console.WriteLine("Input Admin password");
+                admin_log = Console.ReadLine();
+                if (user_input == admin_username && admin_log == admin_password)
+                {
+                    Console.WriteLine("Tell me your name.");
+                    user_name = Console.ReadLine();
 
-                Console.WriteLine("Tell me your phone number " + user_name);
-                user_phonenumber = Console.ReadLine();
+                    Console.WriteLine("Hello, " + user_name + " Tell me your age, ");
+                    user_age = Console.ReadLine();
 
-                Console.WriteLine("So your name is " + user_name + " you are " + user_age + " years old " + " and your phone number is " + user_phonenumber);
+                    Console.WriteLine("Tell me your phone number " + user_name);
+                    user_phonenumber = Console.ReadLine();
+
+                    Console.WriteLine("So your name is " + user_name + " you are " + user_age + " years old " + " and your phone number is " + user_phonenumber);
 
 
-                Console.ReadLine();
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.ReadLine();
+                }
             }
-            else
-            {
+            else {
+                Console.WriteLine("You Lose");
                 Console.ReadLine();
             }
         }
